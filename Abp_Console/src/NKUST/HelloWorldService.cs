ba795f4f822f3127ed0b0c500e9d30809b90711d
yinchang0626 <yinchang0626@gmail.com>
2022-03-24 09:43:12 +0800

    move fold

--- Abp_Console/src/NKUST/HelloWorldService.cs
+++ Abp_Console/src/NKUST/HelloWorldService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Volo.Abp.DependencyInjection;
+using System.Linq;
 
 namespace NKUST;
 
@@ -9,13 +10,35 @@ public class HelloWorldService : ITransientDependency
 {
     public ILogger<HelloWorldService> Logger { get; set; }
 
+    public SemesterService SemesterService { get; set; }
+
     public HelloWorldService()
     {
         Logger = NullLogger<HelloWorldService>.Instance;
+
+        SemesterService=new SemesterService();
     }
 
     public Task SayHelloAsync()
     {
+        Enumerable
+            .Range(1, 10)
+            .ToList()
+            .ForEach(i =>
+            {
+                SemesterService.AddStudent(new Student()
+                {
+                    No=System.DateTime.Now.Millisecond.ToString(),
+                    FullName="Chang-"+i.ToString()
+                });
+            });
+
+        SemesterService.PrintStudentName();
+
+
+
+
+
         Logger.LogInformation("Hello World!");
         return Task.CompletedTask;
     }
---
+++ Abp_Console/src/NKUST/StudentsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKUST
+{
+    public class SemesterService
+    {
+        public SemesterService()
+        {
+            Students=new List<Student>();
+        }
+
+        public List<Student> Students { get; set; }
+
+        public void AddStudent(Student student)
+        {
+            this.Students.Add(student);
+        }
+
+        public void PrintStudentName() 
+        {
+            Students.ForEach(x =>
+            {
+                Console.WriteLine($"學生姓名：{x.FullName}");
+            });
+           
+        }
+    }
+}
