ab4421d4b4673829474044650f8cb89b1a30508c
yinchang0626 <yinchang0626@gmail.com>
2022-03-31 11:49:34 +0800

    add Courses Sample

---
+++ Abp_Console/src/NKUST/Courses/CoursesManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace NKUST.Courses
+{
+    public class CoursesManager : ITransientDependency
+    {
+
+        public async Task<List<Course>> ReadFileAsync(int count = int.MaxValue)
+        {
+            var result = new List<Course>();
+
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            var filePath = System.IO.Path.Combine(basePath, @"Files/大學開課表.csv");
+
+            var lines = await System.IO.File.ReadAllLinesAsync(filePath);
+
+            var headerColumns = lines.First().Split(',').ToList();
+
+            result =lines
+                .Skip(1)
+                .Take(count)
+                .Select(line =>
+            {
+                var datas = line.Split(',');
+
+                var item = new Course();
+
+                headerColumns.ForEach(column =>
+                {
+                    try
+                    {
+                        var index = headerColumns.IndexOf(column);
+
+                        var propertyInfo = typeof(Course).GetProperty(column);
+
+                        if (datas.Length>=index+1)
+                        {
+                            propertyInfo.SetValue(item, datas[index]);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+
+                });
+
+                return item;
+
+                //return new Course()
+                //{
+                //    開課年度= datas[0],
+                //    開課學期= datas[1],
+                //    校名= datas[2],
+                //    課程名稱= datas[3],
+                //    系所名稱= datas[4],
+                //    年級= datas[5],
+                //    開課老師= datas[6],
+                //    學分數= datas[7],
+                //    學制= datas[8],
+                //    必選修= datas[9],
+                //    總修課人數=datas[10]
+                //};
+
+            }).ToList();
+
+
+
+
+            return result;
+
+        }
+    }
+}
--- Abp_Console/src/NKUST/HelloWorldService.cs
+++ Abp_Console/src/NKUST/HelloWorldService.cs
@@ -4,44 +4,57 @@ using Microsoft.Extensions.Logging.Abstractions;
 using Volo.Abp.DependencyInjection;
 using System.Linq;
 using System;
+using NKUST.Courses;
 
 namespace NKUST;
 
 public class HelloWorldService : ITransientDependency
 {
     private readonly ISemesterService semesterService;
+    private readonly CoursesManager coursesManager;
 
     public ILogger<HelloWorldService> Logger { get; set; }
 
     public HelloWorldService(
-        ISemesterService semesterService
+        ISemesterService semesterService,
+        NKUST.Courses.CoursesManager coursesManager
         )
     {
         Logger = NullLogger<HelloWorldService>.Instance;
         this.semesterService=semesterService;
+        this.coursesManager=coursesManager;
     }
 
-    public Task SayHelloAsync()
+    public async Task SayHelloAsync()
     {
-        Enumerable
-            .Range(1, 10)
-            .ToList()
-            .ForEach(i =>
-            {
-                semesterService.AddStudent(new Student()
-                {
-                    No=System.DateTime.Now.Millisecond.ToString(),
-                    FullName="Chang-"+i.ToString()
-                });
-            });
 
-        semesterService.PrintStudentName();
+        var coursees = await coursesManager.ReadFileAsync();
 
 
-        
+        var schoolCount = coursees.GroupBy(x => x.校名).Count();
+
+        var teacherCount = coursees.GroupBy(x => x.開課老師).Count();
+
+        var s = coursees
+            .GroupBy(x => x.校名)
+            .Where(x => x.Key.Contains("國立交通大學"))
+            .Single();
+
+
+        var c = s.ToList();
+
+
+        Logger.LogInformation("Courses Count:"+coursees.Count().ToString());
+
+
+
+
+        coursees.ForEach(x =>
+        {
+            Logger.LogInformation(x.ToString());
+        });
 
 
         Logger.LogInformation("Hello World!");
-        return Task.CompletedTask;
     }
 }
